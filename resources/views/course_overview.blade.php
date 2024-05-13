c9ea17bd1c6020920a7383741563d28f91183216
HANIP <haniffadhlurrahman1999@gmail.com>
2024-05-14 06:37:59 +1000

    add 1 more line

--- resources/views/course_overview.blade.php
+++ resources/views/course_overview.blade.php
@@ -86,7 +86,7 @@
 
                 {{-- Accordion for STATE FILTER HEADER --}}
                 <h2 id="accordion-collapse-heading-2">
-                    <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right border border-gray-200 focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 gap-3 border-b-0" data-accordion-target="#accordion-collapse-body-2" aria-expanded="true" aria-controls="accordion-collapse-body-2">
+                    <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right border border-gray-200 focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-2" aria-expanded="true" aria-controls="accordion-collapse-body-2">
                         <span class="text-purple_template pb-2 text-lg font-semibold">State Filters</span>
                         <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
